8c6c8c97bdb5a640deab4a368f147e07efeb8ae9
Pedro Prieto <pedro.prieto003@gmail.com>
2019-11-24 22:34:20 -0500

    Snacks Class Done

--- src/SnackBar/Main.java
+++ src/SnackBar/Main.java
@@ -2,6 +2,8 @@ package SnackBar;
 
 class Main{
   public static void main(String[] args){
+    Snacks snack1 = new Snacks("Doritos",10,1.99,3);
     System.out.println("Hello World");
+    System.out.println(snack1);
   }
 }
---
+++ src/SnackBar/Snacks.java
@@ -0,0 +1,78 @@
+package SnackBar;
+
+class Snacks{
+
+
+  // PROPERTIES
+  private static int maxId = 0;
+  private int id;
+  private String name;
+  private int quantity;
+  private double cost;
+  private int vendingMachineId;
+
+
+  //CONSTRUCTOR
+  public Snacks(String name, int quantity, double cost, int vendingMachineId){
+    maxId++;
+    id = maxId;
+    this.name = name;
+    this.quantity = quantity;
+    this.cost = cost;
+    this.vendingMachineId = vendingMachineId;
+  }
+
+  /////GETTERS 
+  public int getId(){
+    return id;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public int getQuantity(){
+    return quantity;
+  }
+
+  public double getCost(){
+    return cost;
+  }
+
+  public int getVendingMachineId(){
+    return vendingMachineId;
+  }
+
+
+  ////// SETTERS
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public void setQuantity(int quantity){
+    this.quantity = quantity;
+  }
+
+  public void setCost(double cost){
+    this.cost = cost;
+  }
+
+  public void setVendingMachineId(int vendingMachineId){
+    this.vendingMachineId = vendingMachineId;
+  }
+
+
+
+  ////// TO STRING 
+
+  @Override
+  public String toString(){
+    String rtnString = "id: " + id + "\n" +
+                      "name: " + name + "\n" +
+                      "quantity: " + quantity + "\n" +
+                      "cost: " + cost + "\n" +
+                      "vendingMachineId: " + vendingMachineId + "\n";
+    return rtnString;
+  }
+}
