230d88a3e4a7ad0979100ba83d747ec2934d84d8
Pedro Prieto <pedro.prieto003@gmail.com>
2019-11-26 20:37:33 -0500

    Initial Setup

---
+++ src/test/java/com/lambdaschool/webemployees/WebEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.webemployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEmployeesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
